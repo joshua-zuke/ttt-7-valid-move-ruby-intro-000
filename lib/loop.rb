bd1eca8f59de0154109d8efef5f8ecd9a8160eb6
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-18 17:00:55 +0000

    Automatically backed up by Learn

---
+++ lib/loop.rb
@@ -0,0 +1,7 @@
+
+number_of_hotdogs_eaten = 0
+until number_of_hotdogs_eaten == 7
+  number_of_hotdogs_eaten += 1
+puts "You have eaten #{number_of_hotdogs_eaten} hotdogs."
+end
+puts "You ate #{number_of_hotdogs_eaten} hotdogs."
